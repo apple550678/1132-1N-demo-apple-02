82ff56bfd2176a6bb777ec0bfabba39ccded84c1
apple550678 <apple0974199326@gmail.com>
2025-02-27 20:29:39 +0800

    ideo: W02-P2: show winning message and tie message

--- demo/w02_tictactoe_starter_02/tictactoe_xx.js
+++
@@ -1,9 +0,0 @@
-const o = 'o';
-const x = 'x';
-let turn = 0;
-let done = false;
-
-const container = document.querySelector('.container');
-const alert = document.querySelector('.alert');
-const allLi = document.querySelectorAll('.board li');
-const resetBtn = document.querySelector('.reset');
