7d740df6dcacea494bfaa69bdd42bc1f32b04222
apple550678 <apple0974199326@gmail.com>
2025-03-13 20:18:31 +0800

    Video: W04-P3: Show sdata statistics from js code

--- demo/w04_basics_02/p2_02/p2_1_02.js
+++ demo/w04_basics_02/p2_02/p2_1_02.js
@@ -4,19 +4,61 @@ console.log('sdata', sdata)
 const result1 = document.querySelector('.result1')
 const result2 = document.querySelector('.result2')
 
+const computePass = (s) => {
+  let pass = 0
+  s.forEach((score) => {
+    if (score >= 59.5) pass++
+  })
+  return pass
+}
+
+const computeFail = (s) => {
+  let fail = 0
+  s.forEach((score) => {
+    if (score < 59.5) fail++
+  })
+  return fail
+}
+
+const computeHighest = (s) => {
+  let highest = -100
+  s.forEach((score) => {
+    if (score > highest) highest = score
+  })
+  return highest
+}
+
+const computeLowest = (s) => {
+  let lowest = 200
+  s.forEach((score) => {
+    if (score < lowest) lowest = score
+  })
+  return lowest
+}
+
+const computeSta1 = (s) => {
+  let sum = 0
+  s.forEach((score) => {
+    // sum = sum + score
+    sum += score
+  })
+  return sum / s.length
+}
+
 const outputStat1 = (s) => {
   result1.innerHTML = `
     <h3 class='my-4'> Array Statistics </h3>
     <p>sdata original:</p>
-    <p>[90, 100, 60, 40, 20, 80]</p>
-    <p>Total: 6</p>
-    <p>Pass: 4</p>
-    <p>Fail: 2</p>
-    <p>Highest: 100</p>
-    <p>Lowest: 20</p>
-    <p>Average: 65.2</p>
-
+    <p>${JSON.stringify(s)}</p>
+    <p>Total: ${s.length}</p>
+    <p>Pass: ${computePass(s)}</p>
+    <p>Fail: ${computeFail(s)}</p>
+    <p>Highest: ${computeHighest(s)}</p>
+    <p>Lowest: ${computeLowest(s)}</p>
+    <p>Average: ${computeSta1(s).toFixed(1)}</p>
     `
 }
+
+// sdata.push(85)
 
 outputStat1(sdata)
