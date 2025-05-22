2af7bf6c692c9ee38e083155d1fc9a485f6bfcfc
apple550678 <apple0974199326@gmail.com>
2025-05-22 20:28:34 +0800

    Video: W14-P2: Show static products page

--- demo/w14_fullstack_02/server_02/server_02.js
+++ demo/w14_fullstack_02/server_02/server_02.js
@@ -5,6 +5,14 @@ const app_02 = express()
 app_02.use(express.static('public'))
 app_02.set('view engine', 'ejs')
 
+app_02.use('/product_02/static', (req, res, next) => {
+  res.render('product_02/static_02', {
+    title: 'Get Products - Static',
+    name: '李國蘋',
+    id: '213410102',
+  })
+})
+
 app_02.use('/', (req, res, next) => {
   res.render('index', {
     title: 'Express',
