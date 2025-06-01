92046804e7755ca4acaeaa5e29f03f88bc18ad93
apple550678 <apple0974199326@gmail.com>
2025-06-01 19:56:15 +0800

    Video: W15-P4: Implement route /product_xx to get json data from PostgreSQL server and display them in ejs file

--- demo/w15_fullstack_02/server_02/router/ProductRouter_02.js
+++ demo/w15_fullstack_02/server_02/router/ProductRouter_02.js
@@ -6,9 +6,10 @@ import db from '../utils/database.js'
 router.get('/', async (req, res, next) => {
   try {
     const results = await db.query(`select * from product_02`)
-    // console.log('results', JSON.stringify(results.rows))
+    console.log('results', JSON.stringify(results.rows))
     // res.json(results.rows)
     res.render('product_02/index', {
+      data: results.rows,
       title: 'Get Products From PostgreSQL Server',
       name: '李國蘋',
       id: '213410102',
